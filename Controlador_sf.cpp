a574058be9bbdf138e331a5a39ab9fffabe0b658
Jorge <alu0101410654@ull.edu.es>
2026-03-13 23:22:25 +0000

    1) Se generó el código C++ y con un bloque S-Function se comparó las señales de salida obtenidas (Son iguales). 2) Debido a un warning sobre los loops en el proyecto, se añadió dos "unit delays" en las dos retroalimentaciones.

---
+++ Controlador_sf.cpp
@@ -0,0 +1,13 @@
+/*
+* Generated S-function Target for model Controlador.
+* This source file provides access to the generated S-function target
+* for other models.
+*
+* Created: Fri Mar 13 22:57:22 2026
+*/
+
+#include "Controlador_sf.h"
+#include "Controlador_sfcn_rtw\Controlador_sf.cpp"
+#include "Controlador_sfcn_rtw\Controlador_sf_data.cpp"
+
+
